32b69ec0c61eb9df159c1eca86e371ca22c9ce75
Turbobot <turbobot@vercel.com>
2025-06-18 22:57:03 +0530

    feat(create-turbo): apply official-starter transform

--- backend/src/index.ts
+++
@@ -1,9 +0,0 @@
-
-import app from "./app";
-import { logger } from "./utils/logger";
-const PORT = process.env.PORT || 5000;
-// Start the server
-app.listen(PORT, () => {
-logger.info(`Server running on port ${PORT}`);
-});
-
