2b67580e91744b535a04c436db0a2693cc287a7f
prathamranjansingh <prathamranjan3@gmail.com>
2025-06-19 21:02:42 +0530

    Added Prisma Old Schema

---
+++ packages/prisma/edge.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import dotenv from "dotenv";
+
+dotenv.config();
+const connectionString = `${process.env.NEON_DATABASE_URL}`;
+
+const adapter = new PrismaNeon({ connectionString });
+const prisma = new PrismaClient({ adapter });
