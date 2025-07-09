0b62fac705742fe52294e1410150f4bbb63c91ba
prathamranjansingh <prathamranjan3@gmail.com>
2025-07-09 23:50:09 +0530

    Sidebar components added

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -3,6 +3,14 @@ export * from "./components/ui/button";
 export * from "./components/ui/input";
 export * from "./components/ui/spinner";
 export * from "./components/ui/sonner";
+export * from "./components/ui/dropdown-menu";
+export * from "./components/ui/collapsible";
+export * from "./components/ui/sidebar";
+export * from "./components/ui/tooltip";
+export * from "./components/ui/skeleton";
+export * from "./components/ui/separator";
+export * from "./components/ui/sheet";
+
 //icons
 export * from "./icons/GoogleLogo";
 export * from "./icons/GithubLogo";
